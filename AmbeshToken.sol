8c7cf88ecf9a216c1a03004fa3a7a3b30e4f2753
Ambeshdubey <153484734+Ambeshdubey@users.noreply.github.com>
2024-06-28 11:23:06 +0530

    AmbeshToken.sol

---
+++ AmbeshToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AmbeshToken is ERC20 {
+    address admin;
+
+    constructor() ERC20("Ambesh", "AMB") {
+        admin = msg.sender;
+        _mint(address(this), 100 * 10 ** decimals());
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "This does not belong to the owner");
+        _;
+    }
+
+    function createTokens(address recipient, uint256 quantity) public onlyAdmin {
+        uint balance = balanceOf(address(this));
+        require(balance >= quantity, "Not sufficient balance");
+        _transfer(address(this), recipient, quantity);
+    }
+
+    function destroyTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
